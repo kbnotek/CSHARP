7d9d8762edf0052930bfd84c7b290b2a10f63d45
kbnotek <bruno.q@aluno.senai.br>
2022-07-08 09:05:40 -0300

    Adicionando Projetos elaborado no laboratorio SENAI

---
+++ ExerciciosProRomulo.ExercicioSeis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProRomulo.ExercicioSeis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //###==================== PRIMEIRA FORMA USANDO O FOR =====================================###
+            /* int num, i;
+              num = 1;
+              i = 100;
+              for (int x = num; x <= i; x++)
+              {
+                  if (x % 2 == 0)
+                  {
+                      Console.WriteLine(x);
+                  }
+
+              }*/
+
+            //============================================== USANDO A FORMA DO PROFESSOR USANDO [ WHILE,  IF,  % ] =======================
+
+            int i;
+            i = 1;
+            while (i <= 100)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+              
+                i++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.EscolinhaTicTic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando variável
+            string nome, resultado;
+            double nota1;
+            double nota2;
+            double nota3;
+            double soma;
+            double media;
+            // usando as variaveis 
+            Console.WriteLine("Qual Nome do Aluno?");
+            nome = Console.ReadLine();
+            Console.WriteLine("quais sao as 3 notas ?");
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+            nota3 = double.Parse(Console.ReadLine());
+            soma = nota1 + nota2 + nota3;
+            media = soma / 3;
+            /* if (media >= 7)
+             {
+                 resultado = "Aprovado";
+                 //Console.WriteLine($"Você foi Aprovado sua média é {media}");
+
+             }
+             else
+                 resultado = "Reprovado";
+              //Console.WriteLine($"Você está reprovado sua média é {media} ");
+             */
+
+            //Comando IF ELSE  em line "Operador ternario" .
+            resultado = media >= 7 ? "Aprovado" : "Reprovado";
+            Console.WriteLine($"Nome do Aluno {nome} \r\n As soma das Notas {soma}\r\n Valor da Media {media} e Você foi {resultado}");
+
+           
+        }
+
+    }
+}
